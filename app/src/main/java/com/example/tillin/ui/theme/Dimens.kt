29acfee92d3d99b2c491eb2bd940ae0e0120eb57
sermyu <kyhee2001@naver.com>
2026-02-03 23:28:33 +0900

    chore: dimens 설정

---
+++ app/src/main/java/com/example/tillin/ui/theme/Dimens.kt
@@ -0,0 +1,18 @@
+package com.example.tillin.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val Nano = 5.dp
+    val Tiny = 10.dp
+    val Small = 15.dp
+    val Medium = 20.dp
+    val Large = 22.dp
+    val XLarge = 30.dp
+    val XXLarge = 50.dp
+
+    val DefaultCornerRadius = 10.dp
+    val ButtonCornerRadius = 15.dp
+    val TILCornerRadius = 20.dp
+    val TabBarCornerRadius = 30.dp
+}
