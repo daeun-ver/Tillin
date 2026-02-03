86348f758668931ce10ef6ccbc1e1fbf0a871101
sermyu <kyhee2001@naver.com>
2026-02-03 23:28:39 +0900

    chore: color 설정

--- app/src/main/java/com/example/tillin/ui/theme/Color.kt
+++ app/src/main/java/com/example/tillin/ui/theme/Color.kt
@@ -2,6 +2,19 @@ package com.example.tillin.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+//메인 컬러
+val PrimaryColor = Color(0xFFFF999B)
+val rimaryBackground = Color(0xFFFFF4EC)
+
+//서브 컬러
+val PrimaryHalfColor = Color(0xFFFDC3C4)
+val PrimaryTenthColor = Color(0xFFFFE4E4)
+
+//공통 색상
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Gray = Color(0xFFE7E7E7)
+val LightGray = Color(0xFFF5F5F5)
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
