01c0b806a1787781c615d76056e945b2506eca3c
daeun-ver <kyhee2001@naver.com>
2026-02-04 17:43:32 +0900

    feat: entity 생성

--- app/src/main/java/com/example/tillin/data/local/entity/MonthlyStatsEntity.kt
+++ app/src/main/java/com/example/tillin/data/local/entity/MonthlyStatsEntity.kt
@@ -6,11 +6,10 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "weeklystats")
 data class MonthlyStatsEntity (
     @PrimaryKey(autoGenerate = true)
-    val year: Int = 0,
-    val month: Int = 0,
-    val emotionDistribution: Map<String, Int>,
-    val topKeywords: List<String>,
-    val bestDay: String? = "",
-    val worstDay: String? = "",
-    val updateAt: Long? = null
+    val year: Int = 0,                  // 년도
+    val month: Int = 0,                 // 월
+    val topKeywords: List<String>,      // 월간 키워드
+    val bestDay: String? = "",          // 제일 즐거웠던 날
+    val worstDay: String? = "",         // 제일 힘들었던 날
+    val updateAt: Long? = null          // 수정 일시
 )
