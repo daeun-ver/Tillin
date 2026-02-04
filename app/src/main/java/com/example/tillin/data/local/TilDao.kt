e50fa7860bc1333bd6c495e188c8616f4ca3397e
daeun-ver <kyhee2001@naver.com>
2026-02-04 15:35:11 +0900

    feat: database 생성

--- app/src/main/java/com/example/tillin/data/local/TilDao.kt
+++ app/src/main/java/com/example/tillin/data/local/TilDao.kt
@@ -3,6 +3,7 @@ package com.example.tillin.data.local
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.example.tillin.data.local.entity.TilEntity
