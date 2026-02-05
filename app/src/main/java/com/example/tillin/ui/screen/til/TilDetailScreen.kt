8cc04d5328df8c148c53ba0cb05f8f7778001ce8
daeun-ver <kyhee2001@naver.com>
2026-02-06 00:31:03 +0900

    feat: 기본 Screen 생성

---
+++ app/src/main/java/com/example/tillin/ui/screen/til/TilDetailScreen.kt
@@ -0,0 +1,20 @@
+package com.example.tillin.ui.screen.til
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun TilDetailScreen(navController: NavHostController) {
+    Scaffold() { padding ->
+        Column(modifier = Modifier.fillMaxSize()
+            .padding(padding)) {
+            Text("TilDetailScreen")
+        }
+    }
+}
