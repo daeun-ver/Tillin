92a44982942daf8b30b62215ebe71186ed7734d2
daeun-ver <kyhee2001@naver.com>
2026-02-06 00:30:53 +0900

    feat: HomeTabBar생성

--- app/src/main/java/com/example/tillin/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/tillin/ui/screen/home/HomeScreen.kt
@@ -6,15 +6,40 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import com.example.tillin.ui.screen.home.component.HomeTopBar
+import com.example.tillin.ui.screen.stats.week.WeekScreen
+import com.example.tillin.ui.screen.til.TilListScreen
+
+enum class HomeTab(val label: String) {
+    Home("홈"), STATS("통계")
+}
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
-    Scaffold() { padding ->
-        Column(modifier = Modifier.fillMaxSize()
-            .padding(padding)) {
-            Text("HI")
+    var selectedTab by remember { mutableStateOf(HomeTab.Home) }
+    Scaffold(topBar = {
+
+    }) { inner ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(inner)
+        ) {
+            HomeTopBar(
+                selected = selectedTab,
+                onSelected = { selectedTab = it }
+            )
+
+            when (selectedTab) {
+                HomeTab.Home -> TilListScreen()
+                HomeTab.STATS -> WeekScreen()
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/tillin/ui/screen/home/component/HomeTopBar.kt
@@ -0,0 +1,56 @@
+package com.example.tillin.ui.screen.home.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import com.example.tillin.ui.screen.home.HomeTab
+import com.example.tillin.ui.theme.AppTextStyle
+import com.example.tillin.ui.theme.Black
+import com.example.tillin.ui.theme.Dimens
+import com.example.tillin.ui.theme.PrimaryColor
+import com.example.tillin.ui.theme.White
+
+@Composable
+fun HomeTopBar (
+    selected : HomeTab,
+    onSelected: (HomeTab) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    ScrollableTabRow (
+        selectedTabIndex = HomeTab.entries.indexOf(selected),
+        contentColor = Black,
+        containerColor = White,
+        edgePadding = Dimens.Small,
+        indicator = { tabPosition ->
+            TabRowDefaults.Indicator(
+                modifier = Modifier.tabIndicatorOffset(tabPosition[HomeTab.entries.indexOf(selected)]),
+                color = PrimaryColor,
+                height = 3.dp
+            )
+        },
+        divider = {}
+    ) {
+        HomeTab.entries.forEachIndexed { index, tab ->
+            Tab (
+                selected = selected == tab,
+                onClick = { onSelected(tab) },
+                modifier = Modifier.height(50.dp),
+                text = {
+                    Text(
+                        tab.label,
+                        style = if (selected == tab) AppTextStyle.BodySmall else AppTextStyle.BodySmallGray
+                    )
+                }
+            )
+        }
+    }
+}
