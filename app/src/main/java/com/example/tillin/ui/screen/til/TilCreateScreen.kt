785a23d2b88b24ba2961af434889e453e601df68
daeun-ver <kyhee2001@naver.com>
2026-02-07 00:51:03 +0900

    feat: createScreen TopAppBar 생성

--- app/src/main/java/com/example/tillin/ui/screen/til/TilCreateScreen.kt
+++ app/src/main/java/com/example/tillin/ui/screen/til/TilCreateScreen.kt
@@ -3,20 +3,42 @@ package com.example.tillin.ui.screen.til
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TilCreateScreen(
-
     onBack: () -> Unit
 ) {
-    Scaffold() { padding ->
-        Column(modifier = Modifier.fillMaxSize()
-            .padding(padding)) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {},
+                navigationIcon = {
+                    IconButton(onClick = { onBack() }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "뒤로가기"
+                        )
+                    }
+                }
+            )
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
             Text("TilCreateScreen")
         }
     }
