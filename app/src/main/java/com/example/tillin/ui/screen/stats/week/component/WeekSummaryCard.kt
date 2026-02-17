09b1d36cf3f382215baf95f28290af2cfbe24461
daeun-ver <kyhee2001@naver.com>
2026-02-18 03:14:15 +0900

    chore: 미사용 import 삭제

--- app/src/main/java/com/example/tillin/ui/screen/stats/week/component/WeekSummaryCard.kt
+++ app/src/main/java/com/example/tillin/ui/screen/stats/week/component/WeekSummaryCard.kt
@@ -2,7 +2,6 @@ package com.example.tillin.ui.screen.stats.week.component
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
