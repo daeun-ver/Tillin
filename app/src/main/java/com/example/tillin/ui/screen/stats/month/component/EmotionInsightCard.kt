4d442c0c676d4ac4070c1eb95aa58a1c16531b6b
daeun-ver <kyhee2001@naver.com>
2026-02-18 23:44:28 +0900

    feat: EmotionInsightCard UI 생성

---
+++ app/src/main/java/com/example/tillin/ui/screen/stats/month/component/EmotionInsightCard.kt
@@ -0,0 +1,77 @@
+package com.example.tillin.ui.screen.stats.month.component
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.tillin.ui.screen.EmotionToEmoji
+import com.example.tillin.ui.theme.AppTextStyle
+import com.example.tillin.ui.theme.Dimens
+import com.example.tillin.ui.theme.Gray
+import com.example.tillin.ui.theme.White
+
+@Composable
+fun EmotionInsightCard(
+    emotion: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .padding(Dimens.Tiny)
+            .height(130.dp),
+        shape = RoundedCornerShape(Dimens.TILCornerRadius),
+        colors = CardDefaults.cardColors(White),
+        border = BorderStroke(1.dp, Gray)
+
+    ) {
+        Column(
+            modifier = Modifier.padding(Dimens.Large)
+        ) {
+            Text(text = "감정 분석", style = AppTextStyle.BodySmall)
+            Spacer(modifier = Modifier.height(Dimens.Tiny))
+
+            //최고 날
+            Row {
+                Text(
+                    text = EmotionToEmoji(emotion),
+                    style = AppTextStyle.Body,
+                    modifier = Modifier.padding(start = Dimens.Tiny)
+                )
+                Spacer(modifier = Modifier.width(Dimens.Nano))
+                Text(
+                    text = emotion,
+                    style = AppTextStyle.Body,
+                    modifier = Modifier.padding(start = Dimens.Tiny)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(Dimens.Tiny))
+
+            //최저 날
+            Row {
+                Text(
+                    text = EmotionToEmoji(emotion),
+                    style = AppTextStyle.Body,
+                    modifier = Modifier.padding(start = Dimens.Tiny)
+                )
+                Spacer(modifier = Modifier.width(Dimens.Nano))
+                Text(
+                    text = emotion,
+                    style = AppTextStyle.Body,
+                    modifier = Modifier.padding(start = Dimens.Tiny)
+                )
+            }
+        }
+    }
+}
