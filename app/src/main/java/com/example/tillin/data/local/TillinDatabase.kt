fc0c105da4eded8621972b96da74a443eee8d53c
daeun-ver <kyhee2001@naver.com>
2026-02-23 02:59:20 +0900

    feat: type converter 추가

--- app/src/main/java/com/example/tillin/data/local/TillinDatabase.kt
+++ app/src/main/java/com/example/tillin/data/local/TillinDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.tillin.data.local.entity.MonthlyStatsEntity
 import com.example.tillin.data.local.entity.TilEntity
 import com.example.tillin.data.local.entity.WeeklyStatsEntity
@@ -13,6 +14,7 @@ import com.example.tillin.data.local.entity.WeeklyStatsEntity
     version = 1,
     exportSchema = false
 )
+@TypeConverters(DateConverter::class)
 abstract class TillinDatabase : RoomDatabase() {
     abstract fun tilDao(): TilDao
     abstract fun statsDao(): StatsDao
