dbe3c3a7f782e9a0c929fbc4eff0e5216cb7dc69
daeun-ver <kyhee2001@naver.com>
2026-02-24 00:46:14 +0900

    feat: WidgetTextStyle 추가

---
+++ app/src/main/java/com/example/tillin/ui/theme/WidgetTextStyle.kt
@@ -0,0 +1,18 @@
+package com.example.tillin.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+object WidgetTextStyle {
+    val Body = TextStyle (
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp
+    )
+    val BodyTitle = TextStyle (
+        fontFamily = Pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+}
