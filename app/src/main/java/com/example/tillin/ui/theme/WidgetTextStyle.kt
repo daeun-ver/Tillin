081c5723d4c5f1744274e37f5bf4171daf40b7a0
daeun-ver <kyhee2001@naver.com>
2026-02-24 01:04:50 +0900

    chore: 폰트 수정

--- app/src/main/java/com/example/tillin/ui/theme/WidgetTextStyle.kt
+++ app/src/main/java/com/example/tillin/ui/theme/WidgetTextStyle.kt
@@ -1,17 +1,15 @@
 package com.example.tillin.ui.theme
 
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
+import androidx.glance.text.FontWeight
+import androidx.glance.text.TextStyle
 import androidx.compose.ui.unit.sp
 
 object WidgetTextStyle {
     val Body = TextStyle (
-        fontFamily = Pretendard,
         fontWeight = FontWeight.Normal,
         fontSize = 18.sp
     )
     val BodyTitle = TextStyle (
-        fontFamily = Pretendard,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
