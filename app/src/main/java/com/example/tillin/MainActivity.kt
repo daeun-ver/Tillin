42a6fd9ab22b5041d2bcf7acac32e86d9a78515d
daeun-ver <kyhee2001@naver.com>
2026-02-25 12:35:34 +0900

    feat: 다크모드 전환 함수 추가

--- app/src/main/java/com/example/tillin/MainActivity.kt
+++ app/src/main/java/com/example/tillin/MainActivity.kt
@@ -6,10 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.tillin.ui.navigation.AppNavGraph
+import com.example.tillin.ui.theme.LocalIsDarkMode
+import com.example.tillin.ui.theme.LocalThemeHandler
 import com.example.tillin.ui.theme.TillinTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,10 +26,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TillinTheme {
-                val navController = rememberNavController()
-
-                AppNavGraph(navController)
+            var isDarkMode by remember { mutableStateOf(false) }
+            TillinTheme(darkTheme = isDarkMode) {
+                CompositionLocalProvider(
+                    LocalIsDarkMode provides isDarkMode,
+                    LocalThemeHandler provides { isDarkMode = !isDarkMode }
+                ) {
+                    val navController = rememberNavController()
+                    AppNavGraph(navController)
+                }
             }
         }
     }
